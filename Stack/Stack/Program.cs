97343915cf1ecbce1d0a0a67269b4c05f16b9d5f
sergey-boldyrev <sboldyr@mera.ru>
2015-11-26 19:29:01 +0300

    working fine

--- Stack/Stack/Program.cs
+++ Stack/Stack/Program.cs
@@ -3,7 +3,8 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+//using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Stack
 {
@@ -12,9 +13,11 @@ namespace Stack
         static void Main(string[] args)
         {
             #region Setup
-            int i = 28;
-            int j = 10;
-            
+            Double i = 28.5;
+            Double j = 10.7;
+
+
+            #region Hell is made of comments
             //MyStack<int> arr_int = new Stack.MyStack<int>(j);
             
             //arr_int.Push(i);
@@ -35,7 +38,7 @@ namespace Stack
             //Console.WriteLine("stack length: " + arr_int.Count().ToString());
             //arr_int.Top();
             //Console.WriteLine("stack length: " + arr_int.Count().ToString());
-            
+
             /*for (i = 10; i > 0; i--)
             {
                 Console.WriteLine("top element: " + arr_int.Top().ToString());
@@ -43,15 +46,47 @@ namespace Stack
 
             }
             Console.WriteLine("top element: " + arr_int.Top().ToString());*/
-            SqlConnection conn = new SqlConnection ();
-            MyStack<SqlConnection> stack = Init<SqlConnection>(conn);
+            //#endregion
+
+            //SqlConnection conn = new SqlConnection ();
+            //MyStack<SqlConnection> stack = Init<SqlConnection>(conn);
             #endregion
 
-            Console.WriteLine("top element: " + stack.Top().ToString());
-            
-            Console.ReadLine();
+            MyRect rect = new MyRect(i, j);
+            Console.WriteLine("base circle area: " + rect.area.ToString());
+            MyStack<MyRect> stack = Init<MyRect>(rect);
 
+            MyStack<MyRect> stack_new = new MyStack<MyRect>(10);
+            stack_new.Push(rect);
+            MyRect test_rect = stack_new.Top();
+            
+            if (test_rect.CompareTo(rect) == 1)
+            {
+                Console.WriteLine("rectangles are identical");
+            }
+            else
+            {
+                Console.WriteLine("rectangles are NOT identical");
+            }
+
+            #endregion
+
+
+            Console.WriteLine("top stack element area: " + stack_new.Top().area.ToString());
+            Assert.AreEqual(test_rect.area, rect.area);
+            Console.WriteLine("stack size after push and top: " + stack_new.Count().ToString());
+            MyRect test2_rect = stack_new.Pop();
+            //Console.WriteLine("top stack element area: " + stack_new.Top().area.ToString());
+            Assert.AreEqual(test2_rect.area, rect.area);
+            Console.WriteLine("stack size after pop: " + stack_new.Count().ToString());
+
+            #region exit
+            Console.WriteLine("Press 'Enter' to exit app");
+            Console.ReadLine();
+            #endregion
         }
+
+
         public static MyStack<T> Init<T>(T obj) 
             where T : ICloneable
         {
@@ -67,7 +102,7 @@ namespace Stack
 
     }
 
-    class MyStack<T> //where T : ICloneable
+    class MyStack<T> where T : ICloneable
     {
         int max;
         T[] elements;
@@ -109,7 +144,12 @@ namespace Stack
                 //return elements[cur-1];
                 temp = elements[cur - 1];
             }
-            return temp;
+            T result = default(T);
+            if (temp != null)
+            {
+                result = (T)temp.Clone();
+            }
+            return result;
         }
 
         public T Pop()
@@ -122,7 +162,13 @@ namespace Stack
                 cur--;
                 //return element;
             }
-            return temp;
+
+            T result = default(T);
+            if (temp != null)
+            {
+                result = (T)temp.Clone();
+            }
+            return result;
         }
 
         public int Count()
@@ -141,5 +187,40 @@ namespace Stack
         }*/
 
     }
+
+    class MyRect: ICloneable, IComparable
+    {
+        public Double x;
+        public Double y;
+        public Double area;
+
+        public MyRect(Double x, Double y)
+        {
+            this.x = x;
+            this.y = y;
+            this.area = x * y;
+        }
+
+        public object Clone()
+        {
+            MyRect temp = new MyRect(this.x, this.y);
+                //throw new NotImplementedException();
+            return temp;
+
+        }
+
+        public int CompareTo(object obj)
+        {
+            int result = 0;
+            MyRect temp = (MyRect)obj;
+            //throw new NotImplementedException();
+            if (this.x == temp.x && this.y == temp.y && this.area == temp.area)
+            {
+                result = 1;
+            }
+            return result;
+        }
+    }
+
 
 }
