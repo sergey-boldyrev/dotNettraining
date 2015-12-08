b1d42d263e283d684d4b6534dcfda5ebfd85ca6b
Sergey Boldyrev <sergey.boldyrev@outlook.com>
2015-12-09 01:04:38 +0300

    final one

--- BookCardIndex/BookCardIndex/Program.cs
+++ BookCardIndex/BookCardIndex/Program.cs
@@ -18,21 +18,21 @@ namespace BookCardIndex
     {
 
         enum Ser_type { BIN, SOAP, XML };
-        
+
         static void Main(string[] args)
         {
             #region App menu
             Console.WriteLine("1. List all books");
             Console.WriteLine("2. Add new book");
             Console.WriteLine("Enter index from menu above");
-            //Console.WriteLine("3. List books");
-            //Console.WriteLine("1. List books");
             string choice = Console.ReadLine();
             int choice_int = 0;
+            /*
             String uri_path = @AppDomain.CurrentDomain.BaseDirectory; //System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
             string localPath = new Uri(uri_path).LocalPath;
             string[] presentBooks = Directory.GetFiles(localPath, "*.*").Where(s => s.EndsWith(".dat") || s.EndsWith(".xml") || s.EndsWith(".soap")).ToArray();
-
+ 
+             */
             string cnStr = ConfigurationManager.ConnectionStrings["my_db"].ConnectionString;
 
 
@@ -42,113 +42,145 @@ namespace BookCardIndex
                 try
                 {
                     cn.Open();
-                    string strSQL = "Select * From books";
+                    string strSQL = "SELECT * FROM books";
                     SqlCommand myCommand = new SqlCommand(strSQL, cn);
-                    SqlDataReader dr = myCommand.ExecuteReader();
-                    while (dr.Read())
+                    //SqlDataReader dr = myCommand.ExecuteReader();
+                    //dr.Close();
+
+                    string strSQL1 = "SELECT COUNT(ID) FROM books";
+                    SqlCommand myCommand1 = new SqlCommand(strSQL1, cn);
+
+
+                    int num_books = (int)myCommand1.ExecuteScalar();
+
+                    if (Int32.TryParse(choice, out choice_int))
                     {
-                        Console.WriteLine("ID: {0} NAME: {1}", dr[0], dr[5]);
+                        switch (choice_int)
+                        {
+                            case 1:
+                                if (num_books > 0)
+                                {
+                                    Console.WriteLine("List all books");
+                                    //ListAllBooks(presentBooks);
+
+
+                                    //SqlCommand myCommand = new SqlCommand(strSQL, cn);
+                                    //SqlDataReader dr = myCommand.ExecuteReader();
+                                    SqlDataReader dr = myCommand.ExecuteReader();
+
+                                    while (dr.Read())
+                                    {
+                                        Console.WriteLine("ID: {0} NAME: {1}", dr[0], dr[5]);
+                                    }
+                                    dr.Close();
+
+
+                                    Console.WriteLine("Enter book index to view details");
+                                    string selection = "";
+                                    int y = 0;
+                                    do
+                                    {
+                                        selection = Console.ReadLine();
+                                    } while (!(Int32.TryParse(selection, out y) && y > 0 && y <= num_books));
+                                    //MyBook selected = SelectBook(selection, presentBooks);
+
+                                    string strSQL2 = "SELECT * FROM books WHERE ID = " + y;
+                                    SqlCommand myCommand2 = new SqlCommand(strSQL2, cn);
+                                    SqlDataReader dr2 = myCommand2.ExecuteReader();
+
+                                    while (dr2.Read())
+                                    {
+                                        Console.WriteLine("ID: {0}, NAME: {1}, AUTHOR1: {2}, AUTHOR2: {3}, AUTHOR3: {4}, PUBLISHED: {5}", dr2[0], dr2[5], dr2[1], dr2[2], dr2[3], dr2[4]);
+                                    }
+                                    dr2.Close();
+
+
+                                    /*
+                                    if (selected.name != "")
+                                    {
+                                        DisplayBook(selected);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Wrong book index");
+                                    }*/
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No books in library");
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("Add new book");
+                                MyBook new_one = AddBook();
+                                //DisplayBook(new_one);
+                                string strSQL3 = string.Format("SELECT * FROM books WHERE NAME = '{0}'", new_one.name);
+                                SqlCommand myCommand3 = new SqlCommand(strSQL3, cn);
+                                SqlDataReader dr3 = myCommand3.ExecuteReader();
+                                bool book_present = dr3.HasRows;
+                                dr3.Close();
+
+                                if (!book_present)//(presentBooks.Contains(localPath + new_one.name) != true)
+                                {
+                                    string fields = "NAME, PUBLISHED";
+                                    string values = "'{0}', {1}";
+
+                                    for(int i = 1; i<=new_one.authors.Count(); i++)
+                                    {
+                                        fields += string.Format(", AUTHOR{0}", i);
+                                        values += string.Format(", '{0}'", new_one.authors[i-1]);
+                                    }
+                                    /*
+                                    if (new_one.authors.Count() >= 2)
+                                    {
+                                        
+                                    }*/
+
+                                    string strSQL4 = string.Format("INSERT INTO books ("+ fields +") VALUES (" + values + ")",
+                                        new_one.name,
+                                        new_one.published);
+                    
+                                    
+                                    SqlCommand myCommand4 = new SqlCommand(strSQL4, cn);
+                                    int added = myCommand4.ExecuteNonQuery();
+                                    Console.WriteLine("New book has been added");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("This book is already present");
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("Wrong argument");
+                                break;
+                        }
                     }
+                    else
+                    {
+                        Console.WriteLine("Wrong argument");
+                    }
+
+
                 }
                 catch (SqlException ex)
                 {
                     Console.WriteLine(ex);
                 }
+
+
                 finally
                 {
-                    cn.Close();//test
+                    cn.Close();
                 }
-            }
-
-            if (Int32.TryParse(choice, out choice_int))
-            {
-                switch (choice_int)
-                {
-                    case 1:
-                        if (presentBooks.Length > 0)
-                        {
-                            Console.WriteLine("List all books");
-                            ListAllBooks(presentBooks);
-                            Console.WriteLine("Enter book index to view details");
-                            string selection = "";
-                            int y = 0;
-                            do
-                            {
-                                selection = Console.ReadLine();
-                            } while (!(Int32.TryParse(selection, out y) && y > 0 && y <= presentBooks.Length));
-                            MyBook selected = SelectBook(selection, presentBooks);
-                            if (selected.name != "")
-                            {
-                                DisplayBook(selected);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Wrong book index");
-                            }
-                        }
-                        else 
-                        {
-                            Console.WriteLine("No books in library");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Add new book");
-                        MyBook new_one = AddBook();
-                        DisplayBook(new_one);
-
-                        if (presentBooks.Contains(localPath + new_one.name) != true)
-                        {
-                            Console.WriteLine("Please enter type of serialization");
-                            foreach (Ser_type iType in Enum.GetValues(typeof(Ser_type)))
-                            {
-                                Console.WriteLine(iType.ToString());
-                            }
-                            string type = "";
-                            Ser_type test;
-                            do
-                            {
-                                type = Console.ReadLine();
-                            } while (!Enum.TryParse<Ser_type>(type, true, out test));
-
-
-                            switch (test)
-                            {
-                                case Ser_type.BIN:
-                                    Serialize(new_one, new_one.name + ".dat", Ser_type.BIN);
-                                    break;
-                                case Ser_type.SOAP:
-                                    Serialize(new_one, new_one.name + ".soap", Ser_type.SOAP);
-                                    break;
-                                case Ser_type.XML:
-                                    Serialize(new_one, new_one.name + ".xml", Ser_type.XML);
-                                    break;
-                                default:
-                                    Console.WriteLine("Wrong argument");
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("This book is already present");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Wrong argument");
-                        break;
-                }
-            }
-            else 
-            {
-                Console.WriteLine("Wrong argument");
             }
 
             #endregion
-            Console.WriteLine("Press enter to exit app");
-            Console.ReadLine();
+            Console.WriteLine("Press any key to exit app");
+            Console.ReadKey();
 
         }
         private static MyBook AddBook()
-        { 
+        {
             string name = "";
             do
             {
@@ -160,11 +192,11 @@ namespace BookCardIndex
 
             do
             {
-                Console.WriteLine("Please add authors of the book separated with commas or pipes");
+                Console.WriteLine("Please add authors of the book separated with commas or pipes, up to 3");
                 authors_str = Console.ReadLine();
             } while (!MyBook.VerifyAuthors(authors_str));
 
-            string[] authors_arr = authors_str.Split(',' , '|');
+            string[] authors_arr = authors_str.Split(',', '|');
             //List<string> authors = new List<string>();
             /*foreach (string item in authors_arr)
             {
@@ -188,7 +220,8 @@ namespace BookCardIndex
             MyBook temp = new MyBook(name, authors_arr, published);
             return temp;
         }
-        
+
+        /*
         private static void DisplayBook(MyBook book)
         {
             Console.WriteLine(book.name + ", " + book.published.ToString());
@@ -198,10 +231,10 @@ namespace BookCardIndex
         private static MyBook SelectBook(string id, string[] Books_arr)
         {
             MyBook tmp_book = default(MyBook);
-                   
-          int j = 0;
-          if (Int32.TryParse(id, out j) && j >= 0 && j <= Books_arr.Length)
-          {
+
+            int j = 0;
+            if (Int32.TryParse(id, out j) && j >= 0 && j <= Books_arr.Length)
+            {
                 switch (Path.GetExtension(Books_arr[j - 1]))
                 {
                     case ".dat":
@@ -214,8 +247,8 @@ namespace BookCardIndex
                         tmp_book = Deserialize(Books_arr[j - 1], Ser_type.XML);
                         break;
                 }
-          }
-          return tmp_book;
+            }
+            return tmp_book;
         }
 
 
@@ -237,7 +270,7 @@ namespace BookCardIndex
                         tmp_book = Deserialize(item, Ser_type.XML);
                         break;
                 }
-                Console.WriteLine( i + " " + tmp_book.name);
+                Console.WriteLine(i + " " + tmp_book.name);
                 i++;
             }
             //return i;
@@ -246,7 +279,7 @@ namespace BookCardIndex
         private static void Serialize(Object objectGraph, string fileName, Ser_type type)
         {
             using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
-            { 
+            {
                 switch (type)
                 {
                     case Ser_type.BIN:
@@ -271,7 +304,7 @@ namespace BookCardIndex
                         break;
                 }
             }
-            
+
         }
 
         private static MyBook Deserialize(string fileName, Ser_type type)
@@ -284,27 +317,27 @@ namespace BookCardIndex
                     case Ser_type.BIN:
                         BinaryFormatter binformatter = new BinaryFormatter();
                         return (MyBook)binformatter.Deserialize(fStream);
-                        //break;
+                    //break;
                     case Ser_type.SOAP:
                         SoapFormatter soapformatter = new SoapFormatter();
                         return (MyBook)soapformatter.Deserialize(fStream);
-                        //break;
+                    //break;
                     case Ser_type.XML:
                         XmlSerializer xmlFormat = new XmlSerializer(typeof(MyBook));
                         return (MyBook)xmlFormat.Deserialize(fStream);
-                        //break;
+                    //break;
                     default:
                         Console.WriteLine("Wrong argument");
                         return null;
-                        //break;*/
+                        //break;
                 }
             }
-        }
+        }*/
     }
 
     [Serializable]
 
-    public class MyBook 
+    public class MyBook
     {
         public string name;
         //public List<string> authors;
@@ -328,7 +361,7 @@ namespace BookCardIndex
             this.authors = authors;
             this.published = published;
         }
-        
+
         public static bool VerifyName(string name)
         {
             //
