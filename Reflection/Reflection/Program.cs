f022664a9b56331d73e2879a4b60f4e09066a787
sergey-boldyrev <sboldyr@mera.ru>
2015-12-09 16:32:16 +0300

    final one

--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace Reflection
 {
@@ -10,6 +12,43 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter FullName for an assembly to be used for Exception type hierarchy building, e.g.");
+            Console.WriteLine("'system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' or ");
+            Console.WriteLine("'mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'");
+            string FullName = Console.ReadLine();
+            
+            //string FullName = "system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+            Assembly info = Assembly.Load(FullName);
+
+
+            //System.Reflection.Assembly info = typeof(Exception).Assembly;
+            Console.WriteLine(info.FullName);
+
+            Type[] types = info.GetTypes();
+            List<string> typesHier;// = new Type[0];
+            foreach (Type type in types)
+            {
+                typesHier = new List<string>(0);
+                for (var current = type; current != null; current = current.BaseType)
+                {
+                    
+                    typesHier.Add(current.Name);
+                    if (current == typeof(Exception))
+                    {
+                        string line = "";
+                        for (int i = typesHier.Count - 1; i != 0; i--)
+                        {
+                            Console.WriteLine(line + typesHier[i]);
+                            line += "======";
+                        }
+                        Console.WriteLine(line + type.Name);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
